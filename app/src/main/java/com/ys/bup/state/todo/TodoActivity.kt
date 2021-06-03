1d9e4babdcc19811b0da49bd3660c3d62a9d49b1
YS <yoonsam86@gmail.com>
2021-06-03 11:09:58 +0900

    Compose 설정 및 빌드 설정 변경, 단방향 데이터 흐름 샘플 데이터 추가

---
+++ app/src/main/java/com/ys/bup/state/todo/TodoActivity.kt
@@ -0,0 +1,11 @@
+package com.ys.bup.state.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ys.bup.state.R
+
+class TodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
