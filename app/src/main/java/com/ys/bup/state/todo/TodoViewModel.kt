d7e63d7ebd304cd9903ee1153f8e6a674748565e
YS <yoonsam86@gmail.com>
2021-06-03 16:07:19 +0900

    뷰모델에서 state 를 사용, edit event 처리 함수 추가

--- app/src/main/java/com/ys/bup/state/todo/TodoViewModel.kt
+++ app/src/main/java/com/ys/bup/state/todo/TodoViewModel.kt
@@ -1,22 +1,54 @@
 package com.ys.bup.state.todo
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
 
-    // state: todoItems
-    private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    val todoItems: LiveData<List<TodoItem>> = _todoItems
+    // private state
+    private var currentEditPosition by mutableStateOf(-1)
+
+    // state
+    var todoItems by mutableStateOf(listOf<TodoItem>())
+        private set
+
+    // state
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     fun addItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!! + listOf(item)
+        todoItems = todoItems + listOf(item)
     }
 
     fun removeItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!!.toMutableList().also {
+        todoItems = todoItems.toMutableList().also {
             it.remove(item)
+        }
+
+        onEditDone()
+    }
+
+    // event: onEditItemSelected
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    // event: onEditDone
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    // event: onEditItemChange
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
+        }
+
+        todoItems = todoItems.toMutableList().also {
+            it[currentEditPosition] = item
         }
     }
 }
